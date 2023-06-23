c485b26bb6a39c62174ee6c69f82b87b59f11b01
Liyandercharles <137478175+Liyandercharles@users.noreply.github.com>
2023-06-23 11:31:56 +0530

    Add files via upload
    dhfjdafj

---
+++ charles2803/FileHandle2.java
@@ -0,0 +1,38 @@
+package charles2803;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class FileHandle2
+{
+	public static void main(String args[]) throws IOException
+	{
+		File obja=new File("D:\\java");
+		if(obja.mkdir())
+			System.out.println("Directory Created");
+		
+		File ob1=new File("D:\\java\\charles.txt");
+		if(ob1.createNewFile())
+			System.out.println("File Created");
+		
+		FileWriter ob=new FileWriter("D:\\java\\charles.txt");
+		ob.write("ECHA RAGHUL");
+		ob.close();
+		System.out.println("File Updated");
+		
+		File obj=new File("D:\\java\\charles.txt");
+		Scanner obj1=new Scanner(obj);
+		while(obj1.hasNext()) // predefined method in Scanner class
+			
+		{
+			String obj2=obj1.nextLine();
+			System.out.println(obj2);
+		}
+		obj1.close();
+			/*	if(obj.delete())
+			System.out.println("Folder Dleted");			
+		*/
+	}
+
+}
